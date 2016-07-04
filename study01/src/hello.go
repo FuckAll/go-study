08c219378b13fbbe04842ba10624ddf4d6f44a64
xiangdong.wu <xiangdong.wu@duitang.com>
2016-07-04 14:05:06 +0800

    ddddddd

--- study01/src/hello.go
+++ study01/src/hello.go
@@ -1,19 +1,21 @@
 package main
 
 import (
-    //be "github.com/astaxie/beego"
-    std "fmt"
-    //"strconv"
+	//be "github.com/astaxie/beego"
+	std "fmt"
+	//"strconv"
 )
+
 const (
-    a = "A"
-    b 
+	a = "A"
+	b
 )
-func main(){
-    //var a int = 65
-    //b := strconv.Itoa(a)
-    std.Println(a)
-    std.Println(b)
-    std.Println(^1)
-    //be.Run("127.0.0.1:8089")
+
+func main() {
+	//var a int = 65
+	//b := strconv.Itoa(a)
+	std.Println(a)
+	std.Println(b)
+
+	//be.Run("127.0.0.1:8089")
 }
