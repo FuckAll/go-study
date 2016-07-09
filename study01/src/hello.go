21409c1f812b28617a5a848ecdc8da9f763d50cc
xiangdong.wu <xiangdong.wu@duitang.com>
2016-07-09 16:58:14 +0800

    good

--- study01/src/hello.go
+++ study01/src/hello.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 	"unsafe"
-	// "github.com/urfave/negroni"
-	// "net/http"
 )
+
+// "net/http"
 
 const (
 	a = "abc"
@@ -17,16 +17,62 @@ const (
 	e int  = 1e10
 )
 
-const {
-	Sunday = iota
-	Monday 
-	Tuesday
+const (
+	Sunday             = 1
+	Monday             = 2
+	Tuesday, Wednesday = iota, iota
+)
+
+const (
+	A, B = iota, iota << 10
+	C, D
+)
+
+const (
+	_        = iota
+	KB int64 = 1 << (10 * iota)
+	MB
+	GB
+	TB
+)
+
+type Color int
+
+const (
+	Black Color = iota
+	Red
+	Blue
+)
+
+func wonder(c Color) {
+
 }
 func main() {
-	fmt.Println(a)
-	fmt.Println(b)
-	fmt.Println(c)
-	fmt.Println(d)
-	fmt.Println(e)
+	Ppointer()
 
+}
+
+func Pointer() {
+	x := 0x12345678
+	p := unsafe.Pointer(&x)
+	n := (*[4]byte)(p)
+	for i := 0; i < len(n); i++ {
+		fmt.Printf("%X", n[i])
+
+	}
+}
+
+func Ppointer() {
+	d := struct {
+		s string
+		x int
+	}{"abc", 100}
+	p := uintptr(unsafe.Pointer(&d))
+	p += unsafe.Offsetof(d.x)
+	fmt.Println(p)
+	p2 := unsafe.Pointer(p)
+	fmt.Println(p2)
+	px := (*int)(p2)
+	*px = 200
+	fmt.Printf("%#v\n", d)
 }
