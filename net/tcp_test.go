353ff86726337b40c19c65b7ec1ea29b3a608860
izgnod <izgnod@gmail.com>
2020-04-22 16:54:00 +0800

    增加limit_listener

--- net/tcp_test.go
+++ net/tcp_test.go
@@ -1,4 +1,4 @@
-package main
+package net
 
 import (
 	"bytes"
@@ -8,43 +8,49 @@ import (
 	"net"
 	"net/http"
 	_ "net/http/pprof"
+	"testing"
+	"time"
 )
 
-func main() {
+func TestTCP(t *testing.T) {
 
 	fmt.Println(int(^uint(0) >> 1))
 	go func() {
 		log.Println(http.ListenAndServe("localhost:6060", nil))
 	}()
 
-	listener, err := net.Listen("tcp", "127.0.0.1:8888")
+	listener, err := net.Listen("tcp", "127.0.0.1:8880")
 	if err != nil {
 		panic(err)
 	}
 
-	go func() {
-		//for i := 0; i < 200000; i++ {
-		//
-		//	if i%1000 == 0 {
-		//		time.Sleep(time.Second * 10)
-		//	}
-		conn, err := net.Dial("tcp", "127.0.0.1:8888")
-		if err != nil {
-			println("dial err:", err.Error())
+	//go func() {
+	//	//for i := 0; i < 200000; i++ {
+	//	//
+	//	//	if i%1000 == 0 {
+	//	//		time.Sleep(time.Second * 10)
+	//	//	}
+	//	conn, err := net.Dial("tcp", "127.0.0.1:8880")
+	//	if err != nil {
+	//		println("dial err:", err.Error())
+	//
+	//	}
+	//	for i := 0; i < 200000; i++ {
+	//		_, err := conn.Write([]byte("这是一个完整的包"))
+	//		if err != nil {
+	//			println("dial err:", err.Error())
+	//		}
+	//		//fmt.Println("write length:", n)
+	//		//}
+	//	}
+	//}()
 
-		}
-		for i := 0; i < 200000; i++ {
-			_, err := conn.Write([]byte("这是一个完整的包"))
-			if err != nil {
-				println("dial err:", err.Error())
-			}
-			//fmt.Println("write length:", n)
-			//}
-		}
-	}()
-
+	var count int
 	for {
+		time.Sleep(time.Second * 10)
 		conn, err := listener.Accept()
+		count++
+		fmt.Println("this is count:", count)
 		if err != nil {
 			println("err accept:" + err.Error())
 		}
