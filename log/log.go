994542b694a41aa5a5f80876d7db84c6a949ea2f
FuckAll <wxdanobody2011>
2020-12-24 09:56:37 +0800

    增加log

---
+++ log/log.go
@@ -0,0 +1,23 @@
+package log
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+var (
+	Trace   *log.Logger // 记录所有日志
+	Info    *log.Logger // 重要的信息
+	Warning *log.Logger // 需要注意的信息
+	Error   *log.Logger // 非常严重的问题
+)
+
+func init() {
+	file, err := os.OpenFile("errors.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalln("failed to open error log file:", err)
+	}
+
+	Trace = log.New(ioutil.Discard, "TRACE: ", log.Ldate|log.Ltime|log.Lshortfile)
+}
